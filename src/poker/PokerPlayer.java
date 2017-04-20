44e00ab2e623996715908936c51476ed56d13fbf
Crischelle Pana <crischelle.pana@ucdconnect.ie>
2017-04-20 08:40:56 +0100

    Revert "most important commit - botnames"
    This reverts commit cd18b45c3ac9812a75f75013edc78f0ea92b6333.

--- src/poker/PokerPlayer.java
+++ src/poker/PokerPlayer.java
@@ -15,13 +15,14 @@ package poker;
 abstract class PokerPlayer {
 	
 	public String player_name;
-	private int chips = 100;
+	public int chips = 100;
 	public boolean round_active = false;
-	private HandOfCards hand;
+	public boolean game_active = false;
+	private HandOfCards myHand;
 	
 	PokerPlayer (String name, DeckOfCards deck){
 		player_name = name;
-		hand = new HandOfCards(deck);
+		myHand = new HandOfCards(deck);
 	}
 	
 	private int raise(int ch){
@@ -35,26 +36,20 @@ abstract class PokerPlayer {
 	
 	public int enterGame(int buyIn){
 		int startingBet = raise(buyIn);
+		game_active = true;
 		round_active = true;
 		
 		return startingBet;
 	}
 
 	public void leaveGame(){
-		round_active = false;
-	}
-	
-	public void leaveRound(){
+		game_active = false;
 		round_active = false;
 	}
 	
 	//return what kind of hand player has
 	private String checkHand(){
-		return hand.getHandType();
-	}
-	
-	public int getChips(){
-		return chips;
+		return myHand.getHandType();
 	}
 
 	abstract int action();
