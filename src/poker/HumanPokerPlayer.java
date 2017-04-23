999a9437ae16c303ff064ac2f73ceb6016824b54
Eoghy <eoghan116@gmail.com>
2017-04-24 00:37:44 +0100

    some work on bets
    I need to fix the loop probably but I haven't looked at Twitter stuff yet.
    I'll do Discard in a bit.

--- src/poker/HumanPokerPlayer.java
+++ src/poker/HumanPokerPlayer.java
@@ -7,7 +7,7 @@
  * 		Eoghan O'Donnell	14464082
  * 		Crischelle Pana 	14366596
  * 
- * © 2017 
+ * Â© 2017 
  * */
 
 package poker;
@@ -38,14 +38,40 @@ public class HumanPokerPlayer extends PokerPlayer {
 			actionMessage += "You need " + toCall + " to call.";
 		}
 		
-		twitter.addToTweet(actionMessage);
-		twitter.completeMessage();
+		boolean needPlayerAction = true;	//loops until something is returned
 		
-		//Parse response
-		//TO DO
-		twitter.parseResponse();
-		
-		return 0;
+		while(needPlayerAction){
+			twitter.addToTweet(actionMessage);
+			twitter.completeMessage();
+			String playerAction = twitter.parseResponse();
+			boolean validAction = true;		//set to false if a player bet is invalid
+			if(playerAction.equals("fold")){	//checks for the word fold for a fold
+				fold();
+				return 0;
+			}
+			else if(playerAction.equals("check"))return bet(0);	//if check, bets nothing
+			else if(playerAction.equals("call")){				//call is simply toCall unless
+				if(getChips()>=toCall)return bet(toCall);		//player doesn't have enough chips
+				else return bet(getChips());					//then they all in
+			}
+			else if(playerAction.contains("raise")){			//raise is raise amount + toCall
+				int playerBet = Integer.parseInt(playerAction.substring(6))+toCall;
+				if(playerBet<=getChips())return bet(playerBet);
+				else validAction = false;
+			}
+			else if(playerAction.contains("bet")){				//bet is bet amount, checks that bet is high enough
+				int playerBet = Integer.parseInt(playerAction.substring(4));
+				if(playerBet<=getChips()&&(playerBet+getChipsInPot())>=betAmount)return bet(playerBet);
+				else validAction = false;
+			}
+			else if(playerAction.equals("all in"))return bet(getChips());	//all in simply all ins
+			else validAction = false;							//we should never hit this
+			if(!validAction){									//sets action message for next loop
+				actionMessage = "You entered an invalid command. Please enter your action, followed by a space and integer if applicable";
+			}
+		}
+		return 0; //this should never be reached but it thinks it's not returning an integer otherwise
+					//and I can''t be bothered to fix that rn
 	}
 
 	@Override
