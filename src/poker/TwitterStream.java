f15143aa8e882bf65ba2c102666ca5ba7c56ed20
Catonkey <dara.callinan@live.ie>
2017-04-24 00:57:47 +0100

    Code for getting player's tweets
    wow it worked

--- src/poker/TwitterStream.java
+++ src/poker/TwitterStream.java
@@ -12,21 +12,29 @@
 
 package poker;
 
+import java.util.ArrayList;
+
+import twitter4j.Paging;
 import twitter4j.Status;
 import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
+import twitter4j.User;
 
 public class TwitterStream {
 	
-	static public final int CHAR_LIMIT = 140;
+	static private final int CHAR_LIMIT = 140;
+	static private final int NUM_TWEETS_TO_CHECK = 20;
+	static private
 	
 	Twitter twitter;
+	User user;
 	Status mostRecent;
 	String toSend;
 	
-	TwitterStream(Twitter twit, Status status){
+	TwitterStream(Twitter twit, Status status, User twitterUser){
 		twitter = twit;
+		user = twitterUser;
 		mostRecent = status;
 		toSend = "";
 	}
@@ -71,7 +79,34 @@ public class TwitterStream {
     	Thread.sleep(20*1000);
 	}
 	
-	public void parseResponse(){
-		//TO DO
+	//Get the most recent status from user that is a reply to latest poker tweet (if it exists)
+	//Limited to 20 tweets
+	public Status getRecentReply() throws TwitterException{
+		//List of statuses
+		ArrayList<Status> statuses = new ArrayList<Status>();
+		//Get all statuses for user
+		String u = user.getScreenName();
+		Paging paging = new Paging(1, NUM_TWEETS_TO_CHECK);
+		statuses.addAll(twitter.getUserTimeline(u, paging));
+		//Check if any status is a reply to latest tweet
+		for(int i = 0; i<statuses.size(); i++){
+			System.out.println(statuses.get(i).getInReplyToStatusId());
+			System.out.println(statuses.get(i).getText());
+			if(statuses.get(i).getInReplyToStatusId()==mostRecent.getId()){
+				mostRecent = statuses.get(i);
+				return statuses.get(i);
+			}
+		}
+		return null;
+	}
+	
+	//Return a string of most recent response to latest tweet
+	public String parseResponse() throws TwitterException, InterruptedException{
+		Status response = null;
+		while(response == null){
+			response = getRecentReply();
+			Thread.sleep(10*1000);
+		}
+		return response.getText();
 	}
 }
